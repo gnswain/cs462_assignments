43b6664d7b39db348d35a435be208fddb763b693
Graham <graham.swain27@gmail.com>
2022-09-20 19:46:45 -0400

    worked on error messages

--- assignment1/merge.sh
+++ assignment1/merge.sh
@@ -3,6 +3,7 @@
 # Graham Swain
 # September 23, 2022
 # Takes the name of three files, merges the first two in the third file.
+# Error code represents how many errors there are.
 
 if [[ $# > 0 && $# < 3 ]]; then
     echo 'usage: merge.sh [FILE FILE FILE]'
@@ -17,15 +18,15 @@ if [[ $# == 0 ]]; then
 
     if [[ -z $file1 ]]; then
         echo 'First file name not supplied'
-        valid=1
+        ((valid++))
     fi
     if [[ -z $file2 ]]; then
         echo 'Second file name not supplied'
-        valid=1
+        ((valid++))
     fi
     if [[ -z $file3 ]]; then
         echo 'Third file name not supplied'
-        valid=1
+        ((valid++))
     fi
 else
     file1=$1
@@ -35,43 +36,43 @@ fi
 
 if ! [[ -e "$file1" ]]; then
     echo "$file1 does not exist"
-    valid=1
+    ((valid++))
 elif [[ -d "$file1" ]]; then
     echo "$file1 is a directory"
-    valid=1
+    ((valid++))
 else
     if ! [[ -r "$file1" ]]; then
         echo "$file1 does not have read permissions"
-        valid=1
+        ((valid++))
     fi
     if ! [[ -w "$file1" ]]; then
         echo "$file1 does not have write permissions"
-        valid=1
+        ((valid++))
     fi
     if ! [[ -x "$file1" ]]; then
         echo "$file1 does not have execute permissions"
-        valid=1
+        ((valid++))
     fi
 fi
 
 if ! [[ -e "$file2" ]]; then
     echo "$file2 does not exist"
-    valid=1
+    ((valid++))
 elif [[ -d "$file2" ]]; then
     echo "$file2 is a directory"
-    valid=1
+    ((valid++))
 else 
     if ! [[ -r "$file2" ]]; then
         echo "$file2 does not have read permissions"
-        valid=1
+        ((valid++))
     fi
     if ! [[ -w "$file2" ]]; then
         echo "$file2 does not have write permissions"
-        valid=1
+        ((valid++))
     fi
     if ! [[ -x "$file2" ]]; then
         echo "$file2 does not have execute permissions"
-        valid=1
+        ((valid++))
     fi
 fi
 
@@ -81,26 +82,26 @@ if [[ -e "$file3" ]]; then
     if [[ $overwrite =~ ^[Yy] ]]; then
         if ! [[ -r "$file3" ]]; then
             echo "$file3 does not have read permissions"
-            valid=1
+            ((valid++))
         fi
         if ! [[ -w "$file3" ]]; then
             echo "$file3 does not have write permissions"
-            valid=1
+            ((valid++))
         fi
         if ! [[ -x "$file3" ]]; then
             echo "$file3 does not have execute permissions"
-            valid=1
+            ((valid++))
         fi
     else
-        valid=1
+        ((valid++))
     fi
 elif [[ $valid == 0 ]]; then
     touch $file3
 fi
 
-if [[ $valid == 1 ]]; then
+if [[ $valid -gt 1 ]]; then
     echo 'Cannot perform merge, please check files'
-    exit 1
+    exit $valid
 fi
 
 echo -e "\n-------------------- $file1 --------------------\n" > $file3
